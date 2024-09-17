aa345292009390a407b9f7b642dd574f1ed5110e
Hasan Şimşek <168709291+hasnnsmskk@users.noreply.github.com>
2024-09-17 20:43:34 +0300

    proje görevleri yapıldı.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,78 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(isPalindrome(-1221));
+        System.out.println(isPerfectNumber(10));
+        System.out.println(numberToWords(563));
+    }
+
+    public static boolean isPalindrome(int number) {
+        number = Math.abs(number);
+        char[] digits = String.valueOf(number).toCharArray();
+        String reversed = "";
+        for (int i = digits.length -1; i>=0; i--) {
+            reversed += digits[i];
+        }
+        return reversed.equalsIgnoreCase(String.valueOf(number));
+    }
+
+
+    public static boolean isPerfectNumber(int number) {
+        if(number < 0)
+            return false;
+
+        int total = 0;
+        for(int i = 1; i <= number / 2; i++) {
+            if(number % i == 0) {
+                total += i;
+            }
+        }
+        return number == total;
+    }
+
+
+    public static String numberToWords(int number) {
+        if (number < 0)
+            return "Invalid Value";
+
+        char[] digits = String.valueOf(number).toCharArray();
+        String numToTest = "";
+        for(char digit: digits){
+            switch (digit) {
+                case '0':
+                    numToTest += "Zero ";
+                    break;
+                case '1':
+                    numToTest += "One ";
+                    break;
+                case '2':
+                    numToTest += "Two ";
+                    break;
+                case '3':
+                    numToTest += "Three ";
+                    break;
+                case '4':
+                    numToTest += "Four ";
+                    break;
+                case '5':
+                    numToTest += "Five ";
+                    break;
+                case '6':
+                    numToTest += "Six ";
+                    break;
+                case '7':
+                    numToTest += "Seven ";
+                    break;
+                case '8':
+                    numToTest += "Eight ";
+                    break;
+                case '9':
+                    numToTest += "Nine ";
+                    break;
+            }
+
+        }
+        return numToTest.trim();
     }
 }
+
